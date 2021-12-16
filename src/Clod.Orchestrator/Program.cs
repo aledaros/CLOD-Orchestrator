c75504026815aff4f576e03a5fece32929ab6e4a
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-16 11:28:47 +0100

    Create EndPoint Rabbit

--- src/Clod.Orchestrator/Program.cs
+++ src/Clod.Orchestrator/Program.cs
@@ -15,11 +15,44 @@ IHost host = Host.CreateDefaultBuilder(args)
                       credential.Password("t0mDd3KRsJkXRV3DXzmCUfRWmDFbFu42");
                   });
                 config.ConfigureEndpoints(context);
-            });
+                config.ReceiveEndpoint("orders_create_client", e =>
+                {
+                    e.Consumer<CreateClientEventConsumer>(context);
+                });
+                config.ReceiveEndpoint("orders_delete_client", e =>
+                {
+                    e.Consumer<DeleteClientEventConsumer>(context);
+                });
+
+                config.ReceiveEndpoint("orders_update_client", e =>
+                {
+                    e.Consumer<UpdateClientEventConsumer>(context);
+                });
+
+                config.ReceiveEndpoint("orders_delete_product", e =>
+                {
+                    e.Consumer<DeleteProductEventConsumer>(context);
+                });
+
+                config.ReceiveEndpoint("orders_new_product", e =>
+                {
+                    e.Consumer<NewProductEventConsumer>(context);
+                });
+
+                config.ReceiveEndpoint("orders_update_product", e =>
+                {
+                    e.Consumer<UpdateProductEventConsumer>(context);
+                });
+
+                config.ReceiveEndpoint("orders_newrefill_product", e =>
+                {
+                    e.Consumer<NewRefillEventConsumer>(context);
+                });
+            }); 
         });
         services.AddMassTransitHostedService();
     })
     .Build();
-//fgoi2i2oio
+
 
 await host.RunAsync();
