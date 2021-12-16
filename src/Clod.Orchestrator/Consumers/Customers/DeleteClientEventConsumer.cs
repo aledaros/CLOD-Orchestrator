d18b807713353c1b51a3f18e422d88fbd13664e9
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-16 16:07:09 +0100

    Create consumers

---
+++ src/Clod.Orchestrator/Consumers/Customers/DeleteClientEventConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Gruppo3.ClientiDTO.Domain.Events;
+using Gruppo3.ClientiDTO.Domain.Commands;
+using MassTransit;
+using Gruppo4.Microservizi.Commands.CommandsDTO.Customers;
+
+namespace Gruppo4.Microservizi.AppCore.Consumers.Customers
+{
+    public class DeleteClientEventConsumer : IConsumer<DeleteClientEvent>
+    {
+
+        public async Task Consume(ConsumeContext<DeleteClientEvent> context)
+        {
+            await context.Publish(new DeleteClient { Id=context.Message.Id} );
+        }
+    }
+}
---
+++ src/Clod.Orchestrator/Consumers/Products/DeleteProductEventConsumer.cs
@@ -0,0 +1,18 @@
+﻿using Gruppo4.Microservizi.Commands.CommandsDTO.Products;
+using MassTransit;
+using Microservices.Ecommerce.DTO.Events;
+
+namespace Gruppo4.Microservizi.AppCore.Consumers.Products
+{
+    public class DeleteProductEventConsumer : IConsumer<DeleteProductEvent>
+    {
+
+        public async Task Consume(ConsumeContext<DeleteProductEvent> context)
+        {
+            await context.Publish(new CreateProduct
+            {
+                Id = context.Message.Id,
+            });
+        }
+    }
+}
