648634a004df29dbc91dbdf3df023306433984d5
paolopiaia <83641900+paolopiaia@users.noreply.github.com>
2021-12-16 16:05:28 +0100

    Add consumer program.cs

--- src/Clod.Orchestrator/Program.cs
+++ src/Clod.Orchestrator/Program.cs
@@ -1,12 +1,26 @@
 using Clod.Orchestrator;
+using Gruppo3.ClientiDTO.Domain.Events;
 using MassTransit;
+using MacNuget.Warehouse.Events;
+
 using IHost = Microsoft.Extensions.Hosting.IHost;
+using Gruppo4.Microservizi.AppCore.Consumers.Customers;
+using Gruppo4.Microservizi.AppCore.Consumers.Warehouse;
+using Gruppo4.Microservizi.AppCore.Consumers.Products;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
         services.AddMassTransit(x => {
+            x.AddConsumer<CreateClientEventConsumer>();
+            x.AddConsumer<NewRefillEventConsumer>();
+            x.AddConsumer<UpdateClientEventConsumer>();
+            x.AddConsumer<DeleteProductEventConsumer>();
+            x.AddConsumer<UpdateProductEventConsumer>();
+            x.AddConsumer<NewRefillEventConsumer>();
+            x.AddConsumer<DeleteClientEventConsumer>();
+
             x.UsingRabbitMq((context, config) =>
             {
                 config.Host("roedeer.rmq.cloudamqp.com", "vpeeygzh",
@@ -14,14 +28,18 @@ IHost host = Host.CreateDefaultBuilder(args)
                       credential.Username("vpeeygzh");
                       credential.Password("t0mDd3KRsJkXRV3DXzmCUfRWmDFbFu42");
                   });
+
+
                 config.ConfigureEndpoints(context);
+
+
                 config.ReceiveEndpoint("orders_create_client", e =>
                 {
-                    e.Consumer<CreateClientEventConsumer>(context);
+                    e.Consumer<CreateClientEventConsumer>();
                 });
-                config.ReceiveEndpoint("orders_delete_client", e =>
+                config.ReceiveEndpoint("orders_product_refill", e =>
                 {
-                    e.Consumer<DeleteClientEventConsumer>(context);
+                    e.Consumer<NewRefillEventConsumer>();
                 });
 
                 config.ReceiveEndpoint("orders_update_client", e =>
@@ -44,10 +62,11 @@ IHost host = Host.CreateDefaultBuilder(args)
                     e.Consumer<UpdateProductEventConsumer>(context);
                 });
 
-                config.ReceiveEndpoint("orders_newrefill_product", e =>
+                config.ReceiveEndpoint("orders_delete_client", e =>
                 {
-                    e.Consumer<NewRefillEventConsumer>(context);
+                    e.Consumer<DeleteClientEventConsumer>(context);
                 });
+
             }); 
         });
         services.AddMassTransitHostedService();
