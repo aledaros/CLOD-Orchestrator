ba96d7070af9450bc5016cf83558c2a93932de76
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2021-12-16 15:30:03 +0100

    Implemented Warehouse Consumers

--- src/Clod.Orchestrator/Program.cs
+++ src/Clod.Orchestrator/Program.cs
@@ -7,48 +7,50 @@ IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
-        services.AddMassTransit(x => {
+        services.AddMassTransit(x =>
+        {
             x.UsingRabbitMq((context, config) =>
             {
                 config.Host("roedeer.rmq.cloudamqp.com", "vpeeygzh",
-                  credential => {
+                  credential =>
+                  {
                       credential.Username("vpeeygzh");
                       credential.Password("t0mDd3KRsJkXRV3DXzmCUfRWmDFbFu42");
                   });
 
-                config.ReceiveEndpoint("update-product-event", e =>
+                config.ReceiveEndpoint("orch-warehouse-update-product-event", e =>
                 {
                     e.Consumer<UpdateProductConsumer>();
                 });
 
-                config.ReceiveEndpoint("new-product-event", e =>
+                config.ReceiveEndpoint("orch-warehouse-new-product-event", e =>
                 {
                     e.Consumer<NewProductConsumer>();
                 });
 
-                config.ReceiveEndpoint("delete-product-event", e =>
+                config.ReceiveEndpoint("orch-warehouse-delete-product-event", e =>
                 {
                     e.Consumer<DeleteProductConsumer>();
                 });
 
-                config.ReceiveEndpoint("update-order-event", e =>
-                {
-                    e.Consumer<UpdateOrderConsumer>();
-                });
-
-                config.ReceiveEndpoint("new-order-event", e =>
+                config.ReceiveEndpoint("orch-warehouse-new-order-event", e =>
                 {
                     e.Consumer<NewOrderConsumer>();
                 });
 
-                config.ReceiveEndpoint("delete-order-event", e =>
+                config.ReceiveEndpoint("orch-warehouse-update-order-event", e =>
+                {
+                    e.Consumer<UpdateOrderConsumer>();
+                });
+
+                config.ReceiveEndpoint("orch-warehouse-delete-order-event", e =>
                 {
                     e.Consumer<DeleteOrderConsumer>();
                 });
 
                 config.ConfigureEndpoints(context);
             });
-            
+
         });
         services.AddMassTransitHostedService();
     })
