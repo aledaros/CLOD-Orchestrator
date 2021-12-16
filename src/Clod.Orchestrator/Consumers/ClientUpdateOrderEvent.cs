9e83277f85fb034173eb107ae6689bbd725ea916
Appiah96 <ebenezer.appiah@stud.tecnicosuperiorekennedy.it>
2021-12-16 12:58:18 +0100

    Orchestrator client service done
    Servizi NewOrderEvent, UpdateOrderEvent, DeleteOrderEvent sviluppati dal gruppo.

---
+++ src/Clod.Orchestrator/Consumers/ClientUpdateOrderEvent.cs
@@ -0,0 +1,38 @@
+﻿using Gruppo3.ClientiDTO.Domain.Commands;
+using Gruppo4MicroserviziDTO.DTOs;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clod.Orchestrator.Consumers
+{
+    public class ClientUpdateOrderEvent : IConsumer<UpdatedOrderEvent>
+    {
+        readonly private IBus _rabbit;
+        public ClientUpdateOrderEvent(IBus rabbit)
+        {
+            _rabbit = rabbit;
+        }
+        public Task Consume(ConsumeContext<UpdatedOrderEvent> context)
+        {
+            //get order
+            var order = context.Message;
+
+            //convert event to command
+            var client = new ClientUpdateOrder
+            {
+                Id = order.Id,
+                IdCliente = order.IdCliente,
+                TotalPrice = order.TotalPrice
+            };
+
+            //publish
+            _rabbit.Publish(client);
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/Clod.Orchestrator/Program.cs
+++ src/Clod.Orchestrator/Program.cs
@@ -1,11 +1,43 @@
-using Clod.Orchestrator;
+using Clod.Orchestrator.Consumers;
 using MassTransit;
 using IHost = Microsoft.Extensions.Hosting.IHost;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<ClientDeleteOrderEvent>();
+            x.AddConsumer<ClientNewOrderEvent>();
+            x.AddConsumer<ClientUpdateOrderEvent>();
+
+            //RabbitMQ
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(
+                    Environment.GetEnvironmentVariable("HOST_RABBIT"),
+                    Environment.GetEnvironmentVariable("VHOST_RABBIT"),
+                    hst =>
+                    {
+                        hst.Username(Environment.GetEnvironmentVariable("USERNAME_RABBIT"));
+                        hst.Password(Environment.GetEnvironmentVariable("PASSWORD_RABBIT"));
+                    });
+                //create
+                cfg.ReceiveEndpoint("Orchestrator-NewOrderEvent", e =>
+                    {
+                        e.ConfigureConsumer<ClientNewOrderEvent>(context);
+                    });
+                cfg.ReceiveEndpoint("Orchestrator-UpdateOrderEvent", e =>
+                {
+                    e.ConfigureConsumer<ClientUpdateOrderEvent>(context);
+                });
+                cfg.ReceiveEndpoint("Orchestrator-DeleteOrderEvent", e =>
+                {
+                    e.ConfigureConsumer<ClientDeleteOrderEvent>(context);
+                });
+            });
+        });
+        services.AddMassTransitHostedService(true);
     })
     .Build();
 
