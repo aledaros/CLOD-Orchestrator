c82c13ba3e9b7898815b8ebbf358c9add99e66ad
Mattia <mattia.ceschin@stud.tecnicosuperiorekennedy.it>
2021-12-17 16:08:43 +0100

    feature simplify
    client and order

--- src/Clod.Orchestrator/Consumers/Client/DeleteClientEventConsumer.cs
+++ src/Clod.Orchestrator/Consumers/Client/DeleteClientEventConsumer.cs
@@ -1,14 +1,27 @@
 ﻿using Gruppo3.ClientiDTO.Domain.Events;
+using Gruppo4.Microservizi.Commands.CommandsDTO.Customers;
 using MassTransit;
+using Microservices.Ecommerce.DTO.Commands;
 
 namespace Clod.Orchestrator.Consumers.Client
 {
     public class DeleteClientEventConsumer : IConsumer<DeleteClientEvent>
     {
-        public Task Consume(ConsumeContext<DeleteClientEvent> context)
+        public async Task Consume(ConsumeContext<DeleteClientEvent> context)
         {
             var client = context.Message;
-            throw new NotImplementedException();
+
+            //group 4
+            await context.Publish(new DeleteClient 
+            { 
+                Id = client.Id 
+            });
+
+            //group 2
+            await context.Publish(new DeleteClientCommands
+            {
+                Id = client.Id
+            });
         }
     }
 }
--- src/Clod.Orchestrator/Consumers/Client/NewClientEventConsumer.cs
+++ src/Clod.Orchestrator/Consumers/Client/NewClientEventConsumer.cs
@@ -1,14 +1,41 @@
 ﻿using Gruppo3.ClientiDTO.Domain.Events;
+using Gruppo4.Microservizi.Commands.CommandsDTO.Customers;
 using MassTransit;
+using Microservices.Ecommerce.DTO.Commands;
 
 namespace Clod.Orchestrator.Consumers.Client
 {
     public class NewClientEventConsumer : IConsumer<CreateClientEvent>
     {
-        public Task Consume(ConsumeContext<CreateClientEvent> context)
+        public async Task Consume(ConsumeContext<CreateClientEvent> context)
         {
             var client = context.Message;
-            throw new NotImplementedException();
+
+            //group 2
+            await context.Publish(new CreateClientCommands
+            {
+
+                Id = client.Id,
+                Name = client.Name,
+                Businessname = client.Businessname
+
+
+            });
+
+            //group 4
+            await context.Publish(new CreateClient
+            {
+                Id = client.Id,
+                Address = client.Address,
+                Businessname = client.Businessname,
+                CF = client.CF,
+                Email = client.Email,
+                Name = client.Name,
+                Piva = client.Piva,
+                Surname = client.Surname,
+                Year = client.Year
+            });
+
         }
     }
 }
